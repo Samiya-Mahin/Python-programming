7951e2800b0baac533d0451dd8f6f3f593e17b7c
Samiya Mahin <mahinghulam@gmail.com>
2022-01-02 13:29:17 +0500

    assignment done

---
+++ datascience assignment-1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment
+
+# ## Task 1
+
+# In[52]:
+
+
+Stanza = """Twinkle, twinkle, little star,
+            How I wonder what you are!
+                    Up above the  world so high,
+                    Like a diamond in the sky.
+Twinkle, twinkle, little star,
+            How Iwonder what you are!"""
+print(Stanza)
+
+
+# ## Task 2
+
+# In[3]:
+
+
+pip --version
+
+
+# ## Task 3
+
+# In[8]:
+
+
+import datetime
+x = datetime.datetime.now()
+print(x)    
+
+
+# ## Task 4
+
+# In[49]:
+
+
+import math
+PI = 3.14;
+r = float(input("Enter the value of radius: "))
+Area = PI*r**2;
+print(Area)
+
+
+# ## Task 5
+
+# In[36]:
+
+
+A = input("Enter the first name: ")
+B = input("Enter the last name: ")
+print(B + " " + A)
+
+
+# ## Task 6 
+
+# In[46]:
+
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+add = int(num1) + int(num2);
+print(add)
+
+
+# # I have complete all tasks.
